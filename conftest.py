296b805c27775f20ca3e77070b789d28ae32f2df
PontoReZZ <36677504+PontoReZZ@users.noreply.github.com>
2022-04-27 16:44:37 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,15 +5,15 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption("--language", action="store", default="en", help="Choose language for site (default: English)")
-    parser.addoption("--headless", action="store_true", default="False", help="Start Chrome with headless mode")
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
-    headless_mode = request.config.getoption("headless")
     options = Options()
-    options.add_argument("headless") if headless_mode == True else ""
     options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
+    options.add_argument("no-sandbox")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--headless")
     browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
