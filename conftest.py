42d15d6cc78ccee2a90ef63d7a2f6729de4159f7
PontoReZZ <36677504+PontoReZZ@users.noreply.github.com>
2022-04-27 16:52:12 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -12,8 +12,6 @@ def browser(request):
     options = Options()
     options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
     options.add_argument("no-sandbox")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--headless")
     browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
